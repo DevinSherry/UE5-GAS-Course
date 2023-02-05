ac418ecea4a1ab3e903c4189b863bfb8b5cedbad
Devin Sherry <DevinSherry16@gmail.com>
2023-02-05 19:12:35 +0100

    Base Gameplay Classes

--- Source/GASCourse/GASCourse.Build.cs
+++ Source/GASCourse/GASCourse.Build.cs
@@ -4,10 +4,14 @@ using UnrealBuildTool;
 
 public class GASCourse : ModuleRules
 {
-	public GASCourse(ReadOnlyTargetRules Target) : base(Target)
+	public GASCourse(ReadOnlyTargetRules target) : base(target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput",
+			"GameplayAbilities", "GameplayTags", "GameplayTasks"
+		});
 	}
 }
---
+++ Source/GASCourse/Public/Game/Character/Player/GASCoursePlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GASCoursePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASCOURSE_API AGASCoursePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
